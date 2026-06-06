df7466692b6e4664e998fb45510bd3bb04d89099
sheikkaheel <sheikhkaheel@gmail.com>
2026-06-06 13:10:36 +0530

    Porject Added

---
+++ my-app/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center bg-gradient-to-r from-blue-900 to-black">
+      <div className="text-3xl font-medium animate-bounce underline bg-gradient-to-r from-blue-400 to-white bg-clip-text text-transparent">
+        Welcome Guyz!
+      </div>
+    </div>
+  );
+}
