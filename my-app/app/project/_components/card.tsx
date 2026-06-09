654ae2ff6ffa3708ba8da4e9e979b88c211fd453
abuArsalan <arsalanshafi911@gmail.com>
2026-06-09 19:24:40 +0530

    created structure for card components

---
+++ my-app/app/project/_components/card.tsx
@@ -0,0 +1,8 @@
+export default function Card(){
+    return(
+	<div className="px-[70px] py-[120px] m-[40px] color text-orange-400 border-black outline-[2px]">
+	    this is card
+	</div>
+    );
+
+};
--- my-app/app/project/page.tsx
+++ my-app/app/project/page.tsx
@@ -1,8 +1,12 @@
+import Card from "./_components/card"
 export default function Projects() {
   return (
+
     <div className="w-screen h-screen flex items-center justify-center bg-gradient-to-r from-cyan-900 to-black">
-      <div className="text-3xl font-medium animate-bounce underline bg-gradient-to-r from-cyan-400 to-white bg-clip-text text-transparent">
-        Our Work
+      <div className="flex">
+        <Card/>
+	<Card/>
+	<Card/>
       </div>
     </div>
   );
