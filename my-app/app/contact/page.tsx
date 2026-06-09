6aeffe1c30c83c2ae745c4b63981ece0bd1793ff
Farhan Mir <work.farhanmir@gmail.com>
2026-06-09 20:37:49 +0530

    fixed gradient warning

--- my-app/app/contact/page.tsx
+++ my-app/app/contact/page.tsx
@@ -2,9 +2,10 @@ import Footer from "@/_components/Footer";
 
 export default function Contact() {
   return (
-    <div className="w-screen relative h-screen flex items-center justify-center bg-gradient-to-r from-red-900 to-black">
-      <div className="text-3xl font-medium animate-bounce underline bg-gradient-to-r from-red-400 to-white bg-clip-text text-transparent">
-        Contact Page
+    <div className="w-screen relative h-screen flex items-center justify-center bg-linear-to-r from-purple-600 to-black">
+      <div className="text-8xl font-serif font-extrabold underline bg-linear-to-r from-green-400 to-yellow-200
+       bg-clip-text text-transparent">
+        Contact Us
       </div>
     </div>
   );
