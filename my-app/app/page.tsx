b0541fd69dd23192eac6447588917ca3cd608483
sheikkaheel <sheikhkaheel@gmail.com>
2026-06-12 22:20:06 +0530

    added lucide-react, modification for home-page

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,20 +1,36 @@
+import { ArrowRightIcon, Circle, LoaderPinwheel } from "lucide-react";
+
 export default async function Page() {
   return (
     <div className="w-full overflow-x-hidden">
       <div className="w-screen h-screen flex flex-row gap-4 p-20 items-center justify-center bg-gray-900">
-        <div className="pl-48 flex flex-col justify-center border border-red-400 w-full h-full">
-          <div>Available for Q3 Projects</div>
-          <div className="text-white text-7xl font-bold">
-            Transforming Ideas into Powerful Digital Solutions
+        <div className="pl-52 text-white pr-28 flex flex-col gap-8 justify-center w-full h-full">
+          <div className="flex justify-center items-center gap-3 border border-gray-600 bg-blue-950 w-64 rounded-2xl p-1 text-white font-bold">
+            <LoaderPinwheel className="animate-ping size-3" />
+            Available for Q3 Projects
           </div>
-          <div>
+          <div className="text-7xl font-bold">
+            Transforming Ideas into{" "}
+            <span className="bg-gradient-to-r from-blue-300 to-purple-300 bg-clip-text text-transparent">
+              Powerful Digital Solutions
+            </span>
+          </div>
+          <div className="text-gray-400">
             We build stable, high-performance web ecosystems, custom mobile
             applications, and enterprise cloud solutions that help ambitious
             startups grow.
           </div>
-          <div></div>
+          <div className="flex gap-4">
+            <button className="flex flex-row p-4 gap-2 font-medium rounded-2xl bg-linear-to-r from-blue-600 to-purple-800">
+              Get A Free Consultation
+              <ArrowRightIcon className="text-white" />
+            </button>
+            <button className="border border-gray-800 font-medium py-4 px-6 rounded-2xl">
+              View Our Work
+            </button>
+          </div>
         </div>
-        <div className="border border-red-400 w-full h-full"></div>
+        <div className=" w-full h-full"></div>
       </div>
     </div>
   );
