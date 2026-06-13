7a5b37a090c465a77cdcffcc5604fd375dbc8ae2
abuArsalan <arsalanshafi911@gmail.com>
2026-06-13 13:02:16 +0530

    finalized card structure and fixed type error

--- my-app/app/project/_components/card.tsx
+++ my-app/app/project/_components/card.tsx
@@ -1,9 +1,16 @@
-export default function Card(props){
+interface CardProps  {
+	name : string;
+	des : string;
+}
+
+
+
+export default function Card({name,des}:CardProps){
     return(
-	<div className=" flex flex-col items-start px-[70px] py-[15px] m-[40px] color bg-linear-to-r from-cyan-900 to-black text-white shadow-black shadow-2xl basis-[40%] rounded-lg">	
+	<div className=" flex flex-col items-start px-[70px] py-[15px] m-[40px] color bg-gradient-to-r from-cyan-900 to-black text-white shadow-black shadow-2xl basis-[40%] rounded-lg">	
 	<div className="outline-[2px] border-white h-[300px] w-[100%]">image goes here</div>
-	<div className="text-white text-[40px]">{props.name}</div>
-	<div className="text-white">{props.des}</div>
+	<div className="text-white text-[40px]">{name}</div>
+	<div className="text-white">{des}</div>
 	</div>
     );
 
