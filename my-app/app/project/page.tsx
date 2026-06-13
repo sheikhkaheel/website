0c8ddc83c2d6fc0c5c336ca703f659b31989aac2
abuArsalan <arsalanshafi911@gmail.com>
2026-06-13 14:27:09 +0530

    fixed footer to bottom

--- my-app/app/project/page.tsx
+++ my-app/app/project/page.tsx
@@ -2,10 +2,10 @@ import Card from "./_components/card"
 export default function Projects() {
   return (
 
-    <div className="w-[100%] h-screen bg-gradient-to-r from-cyan-700 to-black pt-[100px]">
+    <div className="w-[100%]  bg-gradient-to-r from-cyan-700 to-black pt-[100px]">
       <div className="flex border-red outline-[2px] w-[100%] flex items-center justify-center flex-wrap">
-        <Card name = "Full Stack" des = "We provide full end to end full stack web applications"/>
-	<Card name = "Android" des = "We provide native android development"/>
+        <Card name = "Full Stack" des = "Check out our full stack projects"/>
+	<Card name = "Android" des = " check out our android projects"/>
 	<Card/>
 	<Card/>
 	<Card/>
