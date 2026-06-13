375834600319aabcb160dce0fcdf4090bfb3f8ab
abuArsalan <arsalanshafi911@gmail.com>
2026-06-13 07:50:07 +0530

    finalized structure of card component

--- my-app/app/project/_components/card.tsx
+++ my-app/app/project/_components/card.tsx
@@ -1,7 +1,9 @@
-export default function Card(){
+export default function Card(props){
     return(
-	<div className="px-[70px] py-[120px] m-[40px] color text-orange-400 border-black outline-[2px]">
-	    this is card
+	<div className=" flex flex-col items-start px-[70px] py-[15px] m-[40px] color bg-linear-to-r from-cyan-900 to-black text-white shadow-black shadow-2xl basis-[40%] rounded-lg">	
+	<div className="outline-[2px] border-white h-[300px] w-[100%]">image goes here</div>
+	<div className="text-white text-[40px]">{props.name}</div>
+	<div className="text-white">{props.des}</div>
 	</div>
     );
 
--- my-app/app/project/page.tsx
+++ my-app/app/project/page.tsx
@@ -2,9 +2,11 @@ import Card from "./_components/card"
 export default function Projects() {
   return (
 
-    <div className="w-screen h-screen flex items-center justify-center bg-gradient-to-r from-cyan-900 to-black">
-      <div className="flex">
-        <Card/>
+    <div className="w-[100%] h-screen bg-linear-to-r from-cyan-700 to-black pt-[100px]">
+      <div className="flex border-red outline-[2px] w-[100%] flex items-center justify-center flex-wrap">
+        <Card name = "Full Stack" des = "We provide full end to end full stack web applications"/>
+	<Card name = "Android" des = "We provide native android development"/>
+	<Card/>
 	<Card/>
 	<Card/>
       </div>
