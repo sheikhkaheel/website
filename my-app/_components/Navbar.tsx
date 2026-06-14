8efe0ace0c4ecb2d6314024c791be78d835c2413
sheikkaheel <sheikhkaheel@gmail.com>
2026-06-14 13:05:11 +0530

    added responsiveness

--- my-app/_components/Navbar.tsx
+++ my-app/_components/Navbar.tsx
@@ -2,25 +2,55 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isOpen]);
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
 
   const getLinkClass = (path: string) => {
     const isActive = pathname === path;
 
-    const baseClass = "relative px-1 py-2 transition-colors duration-300";
+    const baseClass =
+      "relative px-1 py-2 transition-colors duration-300 block w-max";
 
     const activeClass = isActive
       ? "text-blue-400 font-semibold"
       : "text-white hover:text-blue-400";
 
-    return `${baseClass} ${activeClass} after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 after:bg-blue-400 after:transition-transform after:duration-300 ${isActive ? "after:origin-bottom-left after:scale-x-100" : "hover:after:origin-bottom-left hover:after:scale-x-100"}`;
+    return `${baseClass} ${activeClass} after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 after:bg-blue-400 after:transition-transform after:duration-300 ${
+      isActive
+        ? "after:origin-bottom-left after:scale-x-100"
+        : "hover:after:origin-bottom-left hover:after:scale-x-100"
+    }`;
   };
+
+  const navLinks = [
+    { name: "Home", href: "/" },
+    { name: "Projects", href: "/project" },
+    { name: "Services", href: "/services" },
+    { name: "Contact", href: "/contact" },
+  ];
 
   return (
     <nav className="fixed top-0 left-0 w-full z-50 bg-black/40 backdrop-blur-lg border-b border-white/10">
-      <div className="max-w-7xl mx-auto px-8 h-16 flex items-center justify-between">
+      <div className="max-w-7xl mx-auto px-6 md:px-8 h-16 flex items-center justify-between">
         <h1 className="text-xl font-extrabold tracking-wide bg-linear-to-r from-blue-400 via-indigo-200 to-white bg-clip-text text-transparent select-none">
           Earendel
           <span className="font-light tracking-widest text-xs uppercase block text-blue-300/80 -mt-1 pl-0.5">
@@ -28,24 +58,61 @@ export default function Navbar() {
           </span>
         </h1>
 
-        <div className="flex items-center gap-8 font-medium">
-          <Link href="/" className={getLinkClass("/")}>
-            Home
-          </Link>
-
-          <Link href="/project" className={getLinkClass("/project")}>
-            Projects
-          </Link>
-
-          <Link href="/services" className={getLinkClass("/services")}>
-            Services
-          </Link>
-
-          <Link href="/contact" className={getLinkClass("/contact")}>
-            Contact
-          </Link>
+        <div className="hidden md:flex items-center gap-8 font-medium">
+          {navLinks.map((link) => (
+            <Link
+              key={link.href}
+              href={link.href}
+              className={getLinkClass(link.href)}
+            >
+              {link.name}
+            </Link>
+          ))}
         </div>
+
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          type="button"
+          className="md:hidden text-white hover:text-blue-400 focus:outline-none p-2 z-50"
+          aria-label="Toggle menu"
+        >
+          {/* Centered, balanced box proportions */}
+          <div className="w-6 h-5 flex flex-col justify-between relative">
+            <span
+              className={`w-full h-[2px] bg-current transition-all duration-300 origin-center ${
+                isOpen ? "rotate-45 translate-y-[9px]" : ""
+              }`}
+            />
+            <span
+              className={`w-full h-[2px] bg-current transition-all duration-300 origin-center ${
+                isOpen ? "opacity-0 scale-x-0" : ""
+              }`}
+            />
+            <span
+              className={`w-full h-[2px] bg-current transition-all duration-300 origin-center ${
+                isOpen ? "-rotate-45 -translate-y-[9px]" : ""
+              }`}
+            />
+          </div>
+        </button>
       </div>
+
+      {isOpen && (
+        <div className="fixed inset-x-0 bottom-0 top-16 md:hidden bg-black/95 backdrop-blur-xl border-t border-white/10 h-[calc(100dvh-4rem)] z-40 overflow-y-auto touch-none">
+          <div className="flex flex-col items-start justify-start px-8 py-12 space-y-8 text-lg">
+            {navLinks.map((link) => (
+              <Link
+                key={link.href}
+                href={link.href}
+                className={`${getLinkClass(link.href)} text-xl tracking-wide`}
+                onClick={() => setIsOpen(false)}
+              >
+                {link.name}
+              </Link>
+            ))}
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -4,47 +4,42 @@ import Link from "next/link";
 export default async function Page() {
   return (
     <div className="w-full overflow-x-hidden">
-      <div className="w-screen min-h-screen flex flex-row gap-4 px-28 py-36 items-center justify-center bg-[#030712]">
-        <div className="pl-40 text-white pr-10 flex flex-col gap-8 justify-center w-full h-full">
-          <div className="flex justify-center items-center gap-3 border border-gray-600 bg-blue-950 w-64 rounded-2xl p-1 text-white font-bold">
-            <LoaderPinwheel className="animate-ping size-3" />
-            Available for Q3 Projects
-          </div>
-          <h1 className="text-4xl md:text-6xl lg:text-7xl font-black tracking-tighter text-white leading-[1.1] group select-none pr-28">
+      <div className="w-screen min-h-screen flex lg:flex-row flex-col gap-4 lg:px-28 lg:py-36 lg:items-center lg:justify-center bg-[#030712]">
+        <div className="lg:pl-40 px-4 py-34 text-white lg:py-10 lg:pr-10 flex flex-col gap-12 lg:gap-8 justify-center lg:w-1/2 h-full">
+          <h1 className="text-5xl md:text-6xl lg:text-7xl font-black tracking-tighter text-white leading-[1.1] group select-none lg:pr-28">
             Transforming Ideas into{" "}
-            <span className="block sm:inline bg-gradient-to-r from-blue-400 via-indigo-300 to-violet-400 bg-clip-text text-transparent transition-all duration-500 ease-out group-hover:drop-shadow-[0_0_35px_rgba(99,102,241,0.6)]">
+            <span className="block sm:inline bg-linear-to-r from-blue-400 via-indigo-300 to-violet-400 bg-clip-text text-transparent transition-all duration-500 ease-out group-hover:drop-shadow-[0_0_35px_rgba(99,102,241,0.6)]">
               Powerful Digital Solutions
             </span>
           </h1>
-          <div className="text-gray-400 text-xl">
+          <div className="text-gray-400 lg:text-xl">
             We build stable, high-performance web ecosystems, custom mobile
             applications, and enterprise cloud solutions that help ambitious
             startups grow.
           </div>
-          <div className="flex gap-4">
+          <div className="flex gap-4 justify-between lg:justify-normal">
             <Link
               href="/contact"
-              className="group relative flex items-center gap-3 px-8 py-4 font-semibold text-white rounded-2xl bg-gradient-to-r from-indigo-600 to-violet-600 shadow-[0_0_20px_rgba(99,102,241,0.2)] hover:shadow-[0_0_40px_rgba(99,102,241,0.4)] hover:-translate-y-1 transition-all duration-300 overflow-hidden"
+              className="group relative flex items-center lg:gap-3 px-2 py-4 lg:px-8 lg:py-4 font-semibold text-white rounded-2xl bg-linear-to-r from-indigo-600 to-violet-600 shadow-[0_0_20px_rgba(99,102,241,0.2)] hover:shadow-[0_0_40px_rgba(99,102,241,0.4)] hover:-translate-y-1 transition-all duration-300 overflow-hidden lg:text-sm text-xs"
             >
-              <div className="absolute top-0 left-0 w-full h-[1px] bg-white/30" />
               Get A Free Consultation
-              <ArrowRightIcon className="w-5 h-5 group-hover:translate-x-1.5 transition-transform duration-300 ease-out" />
+              <ArrowRightIcon className="w-5 h-5 group-hover:translate-x-1.5 ml-2 lg:ml-0 transition-transform duration-300 ease-out" />
             </Link>
 
             <Link
               href="/project"
-              className="flex items-center justify-center px-8 py-4 font-semibold text-slate-300 rounded-2xl bg-[#030712]/50 backdrop-blur-md border border-slate-700/50 hover:bg-slate-800/50 hover:text-white hover:border-indigo-500/50 hover:shadow-[0_0_20px_rgba(99,102,241,0.15)] hover:-translate-y-1 transition-all duration-300"
+              className="flex items-center justify-center lg:text-sm text-xs py-4 px-5 lg:px-8 lg:py-4 font-semibold text-slate-300 rounded-2xl bg-[#030712]/50 backdrop-blur-md border border-slate-700/50 hover:bg-slate-800/50 hover:text-white hover:border-indigo-500/50 hover:shadow-[0_0_20px_rgba(99,102,241,0.15)] hover:-translate-y-1 transition-all duration-300"
             >
               View Our Work
             </Link>
           </div>
         </div>
 
-        <div className="relative w-full h-[600px] flex items-center justify-center pointer-events-none">
+        <div className="relative lg:w-1/2 h-[600px] flex items-center justify-center pointer-events-none">
           <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-64 h-64 bg-indigo-500/30 rounded-full blur-[80px]" />
           <div className="absolute top-1/3 left-1/3 w-48 h-48 bg-violet-500/20 rounded-full blur-[60px]" />
 
-          <div className="absolute top-12 right-28 w-72 h-48 bg-[#030712]/60 backdrop-blur-xl border border-indigo-500/20 rounded-xl p-4 shadow-[0_0_40px_rgba(99,102,241,0.1)] transform rotate-3 transition-transform duration-700 hover:rotate-0 hover:scale-105 pointer-events-auto">
+          <div className="absolute top-16 lg:top-12 lg:right-28 w-72 h-48 bg-[#030712]/60 backdrop-blur-xl border border-indigo-500/20 rounded-xl p-4 shadow-[0_0_40px_rgba(99,102,241,0.1)] transform rotate-3 transition-transform duration-700 hover:rotate-0 hover:scale-105 pointer-events-auto">
             <div className="flex items-center justify-between border-b border-indigo-500/10 pb-2 mb-3">
               <div className="flex gap-1.5">
                 <div className="w-2.5 h-2.5 rounded-full bg-slate-700" />
@@ -89,7 +84,7 @@ export default async function Page() {
             </div>
           </div>
 
-          <div className="absolute top-24 left-2 px-4 py-2 bg-[#030712]/60 backdrop-blur-md border border-blue-400/20 rounded-full shadow-lg flex items-center gap-2 transform -translate-y-4 animate-[bounce_4s_infinite] pointer-events-auto">
+          <div className="absolute top-4 lg:top-24 left-2 px-4 py-2 bg-[#030712]/60 backdrop-blur-md border border-blue-400/20 rounded-full shadow-lg flex items-center gap-2 transform -translate-y-4 animate-[bounce_4s_infinite] pointer-events-auto">
             <div className="w-2 h-2 rounded-full bg-blue-400 animate-pulse" />
             <span className="text-xs font-semibold tracking-wide text-blue-200">
               API Connected
